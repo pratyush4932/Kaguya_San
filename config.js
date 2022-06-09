2a8aaf7630dc441a7b11138d797a4f47a1827787
Das-kun <106020431+Das-kun@users.noreply.github.com>
2022-06-09 18:33:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.packname = process.env.PACKNAME || "kaguya",
 global.prefix= process.env.PREFIX || "-",
 global.author = process.env.AUTHOR || "ARUS",
 global.sessionName = 'arus'
-global.menu = '🎫 *Kaguya-San\'s Command Liste* 🎫\n\n💻 *Dev*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n🔮 *General*\n❐ ```groupinfo, help, delete, info, rank, profile```\n\n💠 *Fun*\n❐ ```ship, quote, leaderboard,react, fact, bot```\n\n🔈 *Media*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n👑 *Moderation*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n💮 *Weeb*\n❐ ```anime, manga, charecter, haigusha, neko, pokemon, holo, fox_girl, kemonomimi```\n\n🖇 *Utils*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n🗃️ *Notiz: Nutze -report <text> report the bug if you use it more then 5 times in a row you will be ban*'
+global.menu = '🎫 *Kaguya-San\'s Command Liste* 🎫\n\n💻 *Dev*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n🔮 *General*\n❐ ```groupinfo, help, delete, info, rank, profile```\n\n💠 *Fun*\n❐ ```ship, quote, leaderboard,react, fact, bot```\n\n🔈 *Media*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n👑 *Moderation*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n💮 *Weeb*\n❐ ```anime, manga, charecter, haigusha, neko, pokemon```\n\n🖇 *Utils*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n🗃️ *Notiz: Nutze -report <text> report the bug if you use it more then 5 times in a row you will be ban*'
 global.thumb = fs.readFileSync('./src/info.jpeg')
 
 let file = require.resolve(__filename)
