e59fa1acd1c3a475bb7181e82cb306a8b71e582d
Das-kun <106020431+Das-kun@users.noreply.github.com>
2022-06-10 14:33:24 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -228,8 +228,13 @@ await arus.sendMessage(m.chat,buttonMessaged,{quoted:m})
 }
  break
        case 'grupinfo': case 'groupinfo': case 'group info' :
+	   try {
+	    owne = groupMetadata.owner.split('@')[0]
+	   } catch {
+		    owne = ''
+	   }
 const code = await arus.groupInviteCode(m.chat)
-let ingfo = `*━━━━『🍀Group-Info🍀』━━━━*\n\n*🎐Name:* ${groupName}\n\n*🔩ID Group:* ${m.chat}\n\n*🍀Made:* ${moment(`${groupMetadata.creation}` * 1000).tz('Asia/Kolkata').format('DD/MM/YYYY HH:mm:ss')}\n\n*🥇Group Owner:* @${groupMetadata.owner.split('@')[0]}\n\n*🔍Number Of Admins:* ${groupAdmins.length}\n\n*🎍Number Of Participants:* ${participants.length}\n\n*🔍Desc:* \n\n${groupMetadata.desc}`
+let ingfo = `*━━━━『🍀Group-Info🍀』━━━━*\n\n*🎐Name:* ${groupName}\n\n*🔩ID Group:* ${m.chat}\n\n*🍀Made:* ${moment(`${groupMetadata.creation}` * 1000).tz('Asia/Kolkata').format('DD/MM/YYYY HH:mm:ss')}\n\n*🥇Group Owner:* @${owne}\n\n*🔍Number Of Admins:* ${groupAdmins.length}\n\n*🎍Number Of Participants:* ${participants.length}\n\n*🔍Desc:* \n\n${groupMetadata.desc}`
 //m.reply(ingfo)
 arus.sendMessage(m.chat, { text: ingfo, mentions: [groupMetadata.owner]} , { quoted: m})
 break
