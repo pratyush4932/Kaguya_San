4b0e29b16d0c435d67cb4ffd3ab813792ff72143
Das-kun <106020431+Das-kun@users.noreply.github.com>
2022-06-11 10:58:33 +0530

    Update heart.js

--- heart.js
+++ heart.js
@@ -519,9 +519,14 @@ if (grp.action == 'add' && hh == "true") {
 
 startArus()
 
-app.get('/', async (req, res) => {
-	res.setHeader("content-type", "image/png");
-	res.end(await qrcode.toBuffer(QR_GENERATE));
+app.get('/', (req, res) => {
+            res.setHeader('Content-Type', 'text/plain')
+            res.write('Go to /qr to authenicate')
+            res.end()
+        })
+
+app.get('/qr', async (req, res) => {
+        res.setHeader("content-type", "image/png").send(await qrcode.toBuffer(QR_GENERATE))
 });
 
 app.listen(PORT, () => {
